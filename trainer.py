b6af5b9d33a577907c0a1b2e6b60f18e010e7a1a
Dima <dima@spacestation.local>
2024-10-30 21:04:46 +0200

    layers freezed and hyperparams adjusted

--- trainer.py
+++ trainer.py
@@ -2,24 +2,26 @@ import json
 import os
 
 import torch
+from datasets import load_dataset
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, TextDataset, DataCollatorForLanguageModeling, \
     TrainingArguments, Trainer
 
 
 def train():
     model_name = 'gpt2'
-    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     model = GPT2LMHeadModel.from_pretrained(model_name)
 
+    for param in model.transformer.parameters():
+        param.requires_grad = False
+
+    for param in model.lm_head.parameters():
+        param.requires_grad = True
+
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     tokenizer.pad_token = tokenizer.eos_token
-    corpus_file_path = './data/corpus.txt'
 
-    prep_corpus_file(corpus_file_path)
-
-    dataset = TextDataset(
-        file_path=corpus_file_path,
-        tokenizer=tokenizer,
-        block_size=128,
+    dataset = prep_dataset(
+        tokenizer, './data/corpus.json'
     )
 
     data_collator = DataCollatorForLanguageModeling(
@@ -30,19 +32,22 @@ def train():
     training_args = TrainingArguments(
         output_dir='./output',
         overwrite_output_dir=True,
-        num_train_epochs=3,
+        num_train_epochs=50,
         per_device_train_batch_size=4,
-        save_steps=10000,
-        save_total_limit=2,
-        # learning_rate=2e-5,
+        save_steps=5000,
+        learning_rate=1e-5,
         # weight_decay=0.01,
+        evaluation_strategy="epoch",
     )
+
+    split_dataset = dataset['train'].train_test_split(test_size=0.1)
 
     trainer = Trainer(
         model=model,
         args=training_args,
         data_collator=data_collator,
-        train_dataset=dataset
+        train_dataset=split_dataset['train'],
+        eval_dataset=split_dataset['test'],
     )
 
     trainer.train()
@@ -51,11 +56,37 @@ def train():
     tokenizer.save_pretrained('./model')
 
 
+def prep_dataset(tokenizer, file_path):
+    dataset = load_dataset(
+        "json", data_files=file_path
+    )
+
+    def encode(item):
+        return tokenizer(
+            item['description'], truncation=True, padding="max_length", max_length=256
+        )
+
+    tokenized_dataset = dataset.map(
+        encode, batched=True
+    )
+
+    return tokenized_dataset
+
+
+
+def prep_txt_dataset(tokenizer, file_path):
+    return TextDataset(
+        file_path=file_path,
+        tokenizer=tokenizer,
+        block_size=128,
+    )
+
+
 def prep_corpus_file(file_path: str = './data/corpus.txt') -> None:
     if os.path.exists(file_path):
         return
 
-    file_path_json = file_path.replace(".txt",".json")
+    file_path_json = file_path.replace(".txt", ".json")
     if not os.path.exists(file_path_json):
         raise FileNotFoundError
 
